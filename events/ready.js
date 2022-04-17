680b2448add6907699cb7217bdb808b13673aeae
XBFTW <74849594+XBFTW@users.noreply.github.com>
2022-04-17 13:18:28 -0700

    added pterodactylReady
    pterodactylReady is used with the panel named Pterodactyls, https://pterodactyl.io/ . For the panel to detect that the bot is running it needs this .

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,10 @@ module.exports = {
 	once: true,
 	execute(client) {
 
+		console.log('pterodactylReady')
 		//logs to console when the bot is ready
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
 
 		//sets the activity of the bot
 		client.user.setActivity('xbftw.com', { type: 'WATCHING' });
