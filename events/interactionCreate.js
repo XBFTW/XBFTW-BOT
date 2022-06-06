e38117130a642f8005e9e11f537b606336b41e87
XBFTW <74849594+XBFTW@users.noreply.github.com>
2022-06-05 18:29:07 -0700

    Delete events directory

--- events/interactionCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`${interaction.user.tag} triggered an interaction in #${interaction.channel.name}.`);
-	},
-};
